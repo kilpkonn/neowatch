9af10eff574370c6ced5bffe53067f3139e90fee
Tavo Annus <tavo.annus@gmail.com>
2022-10-06 12:29:00 +0300

    Update to clap v4

--- src/args.rs
+++ src/args.rs
@@ -1,5 +1,5 @@
-use clap::{App, AppSettings, Arg};
-use std::{str::FromStr, time::Duration};
+use clap::{Arg, ArgAction, Command};
+use std::{ffi::OsString, str::FromStr, time::Duration};
 use termcolor::{Color, ColorSpec};
 
 use crate::error::Error;
@@ -17,16 +17,17 @@ pub struct Args {
     pub color_increase: ColorSpec,
     pub color_decrease: ColorSpec,
     pub cmd: String,
-    pub cmd_args: Vec<String>,
+    pub cmd_args: Vec<OsString>,
 }
 
 impl Args {
     pub fn new() -> Result<Self, Error<'static>> {
-        let matches = App::new("neowatch")
+        let matches = Command::new("neowatch")
             .version(env!("CARGO_PKG_VERSION"))
             .author(env!("CARGO_PKG_AUTHORS"))
             .about(env!("CARGO_PKG_DESCRIPTION"))
-            .setting(AppSettings::AllowExternalSubcommands)
+            .arg_required_else_help(true)
+            .allow_external_subcommands(true)
             .arg(
                 Arg::new("interval")
                     .short('n')
@@ -34,94 +35,100 @@ impl Args {
                     .value_name("SECS")
                     .default_value("1.0")
                     .help("Set update insterval")
-                    .takes_value(true),
+                    .num_args(1)
+                    .action(ArgAction::Set),
             )
             .arg(
                 Arg::new("show_diff")
                     .short('d')
                     .long("differences")
                     .help("Highlight differences since last update")
-                    .takes_value(false),
+                    .action(ArgAction::SetTrue),
             )
             .arg(
                 Arg::new("precise")
                     .short('p')
                     .long("precise")
                     .help("Attempt to run command at precise intervals")
-                    .takes_value(false),
+                    .action(ArgAction::SetTrue),
             )
             .arg(
                 Arg::new("exit_on_err")
                     .short('e')
                     .long("errexit")
                     .help("Exit on non-zero return code")
-                    .takes_value(false),
+                    .action(ArgAction::SetTrue),
             )
             .arg(
                 Arg::new("exit_on_change")
                     .short('g')
                     .long("chgexit")
                     .help("Exit on output change")
-                    .takes_value(false),
+                    .action(ArgAction::SetTrue),
             )
             .arg(
                 Arg::new("color_new")
                     .long("new-color")
                     .help("Color for new text [word|ANSI|rr,gg,bb]")
                     .value_name("COLOR")
-                    .takes_value(true),
+                    .num_args(1)
+                    .action(ArgAction::Set),
             )
             .arg(
                 Arg::new("color_change")
                     .long("change-color")
                     .help("Color for changed text [word|ANSI|rr,gg,bb]")
                     .value_name("COLOR")
-                    .takes_value(true),
+                    .num_args(1)
+                    .action(ArgAction::Set),
             )
             .arg(
                 Arg::new("number_diff")
                     .short('z')
                     .long("number-changes")
                     .help("Highlight number changes based on increase/decrease")
-                    .takes_value(false),
+                    .action(ArgAction::SetTrue),
             )
             .arg(
                 Arg::new("radix")
                     .long("radix")
                     .help("Radix for numbers")
                     .default_value("10")
-                    .takes_value(true),
+                    .num_args(1)
+                    .action(ArgAction::Set),
             )
             .arg(
                 Arg::new("color_increase")
                     .long("increase-color")
                     .help("Color for increaseing numeric values")
                     .value_name("COLOR")
-                    .takes_value(true),
+                    .num_args(1)
+                    .action(ArgAction::Set),
             )
             .arg(
                 Arg::new("color_decrease")
                     .long("decrease-color")
                     .help("Color for decreasing numeric values")
                     .value_name("COLOR")
-                    .takes_value(true),
+                    .num_args(1)
+                    .action(ArgAction::Set),
             )
             .get_matches();
 
         let interval = matches
-            .value_of("interval")
+            .get_one::<String>("interval")
             .and_then(|s| s.parse::<f32>().ok())
             .unwrap_or(1.0);
         let interval = Duration::from_secs_f32(interval);
 
-        let show_diff = matches.is_present("show_diff");
-        let precise_mode = matches.is_present("precise_mode");
-        let exit_on_err = matches.is_present("exit_on_err");
-        let exit_on_change = matches.is_present("exit_on_change");
-        let show_number_diff = matches.is_present("number_diff");
+        let show_diff = matches.get_flag("show_diff");
+        let precise_mode = matches.get_flag("precise");
+        let exit_on_err = matches.get_flag("exit_on_err");
+        let exit_on_change = matches.get_flag("exit_on_change");
+        let show_number_diff = matches.get_flag("number_diff");
 
         let radix = matches
-            .value_of("radix")
+            .get_one::<String>("radix")
             .and_then(|s| s.parse::<u32>().ok())
             .unwrap_or(10);
 
@@ -131,22 +138,22 @@ impl Args {
         let mut color_decrease = ColorSpec::new();
 
         let col_new = matches
-            .value_of("color_new")
+            .get_one::<String>("color_new")
             .and_then(|s| FromStr::from_str(s).ok())
             .unwrap_or(Color::Green);
 
         let col_change = matches
-            .value_of("color_change")
+            .get_one::<String>("color_change")
             .and_then(|s| FromStr::from_str(s).ok())
             .unwrap_or(Color::Magenta);
 
         let col_increase = matches
-            .value_of("color_increase")
+            .get_one::<String>("color_increase")
             .and_then(|s| FromStr::from_str(s).ok())
             .unwrap_or(Color::Cyan);
 
         let col_decrease = matches
-            .value_of("color_decrease")
+            .get_one::<String>("color_decrease")
             .and_then(|s| FromStr::from_str(s).ok())
             .unwrap_or(Color::Red);
 
@@ -157,8 +164,8 @@ impl Args {
 
         let (cmd, cmd_args) = match matches.subcommand() {
             Some((external, ext_m)) => {
-                let ext_args = if let Some(args) = ext_m.values_of("") {
-                    args.map(String::from).collect()
+                let ext_args = if let Some(args) = ext_m.get_many::<OsString>("") {
+                    args.map(OsString::to_owned).collect()
                 } else {
                     Vec::new()
                 };
